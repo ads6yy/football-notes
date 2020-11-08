523fc0221b69973937302c3de6e121e0c5e79efc
Adsy <adsysama@gmail.com>
2020-11-08 12:49:40 +0100

    Onglet Composition / Data - Ajout graphique "ballons touchés +/-" "duels gagnés/perdus"

--- web/themes/custom/footballnotes/js/note.js
+++ web/themes/custom/footballnotes/js/note.js
@@ -18,7 +18,7 @@
     attach: function (context, settings) {
 
       // BALLONS TOUCHES.
-      Highcharts.chart('container-touches-all', {
+      Highcharts.chart('container-most-touches-all', {
         chart: {
           type: 'column'
         },
@@ -29,7 +29,7 @@
           }
         },
         xAxis: {
-          categories: drupalSettings.note.touchesTotal.categorie,
+          categories: drupalSettings.note.mostTouchesTotal.categorie,
           title: {
             text: null
           }
@@ -70,13 +70,71 @@
         },
         series: [
           {
-            name: drupalSettings.note.touchesTotal.serieName,
-            data: drupalSettings.note.touchesTotal.serie,
+            name: drupalSettings.note.mostTouchesTotal.serieName,
+            data: drupalSettings.note.mostTouchesTotal.serie,
+            color: '#000000'
+          },
+        ]
+      });
+      Highcharts.chart('container-most-touches-home', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Les joueurs ayant touché le plus de ballons',
+          style: {
+            'display': 'none',
+          }
+        },
+        xAxis: {
+          categories: drupalSettings.note.mostTouchesTotalHome.categorie,
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Ballons touchés',
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: ' ballons touchés'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: drupalSettings.note.mostTouchesTotalHome.serieName,
+            data: drupalSettings.note.mostTouchesTotalHome.serie,
             color: '#D0021B'
           },
         ]
       });
-      Highcharts.chart('container-touches-home', {
+      Highcharts.chart('container-most-touches-away', {
         chart: {
           type: 'column'
         },
@@ -87,7 +145,7 @@
           }
         },
         xAxis: {
-          categories: drupalSettings.note.touchesTotalHome.categorie,
+          categories: drupalSettings.note.mostTouchesTotalAway.categorie,
           title: {
             text: null
           }
@@ -128,24 +186,140 @@
         },
         series: [
           {
-            name: drupalSettings.note.touchesTotalHome.serieName,
-            data: drupalSettings.note.touchesTotalHome.serie,
+            name: drupalSettings.note.mostTouchesTotalAway.serieName,
+            data: drupalSettings.note.mostTouchesTotalAway.serie,
+            color: '#022fd0'
+          },
+        ]
+      });
+      Highcharts.chart('container-less-touches-all', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Les joueurs ayant touché le moins de ballons',
+          style: {
+            'display': 'none',
+          }
+        },
+        xAxis: {
+          categories: drupalSettings.note.lessTouchesTotal.categorie,
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Ballons touchés',
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: ' ballons touchés'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: drupalSettings.note.lessTouchesTotal.serieName,
+            data: drupalSettings.note.lessTouchesTotal.serie,
+            color: '#000000'
+          },
+        ]
+      });
+      Highcharts.chart('container-less-touches-home', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Les joueurs ayant touché le moins de ballons',
+          style: {
+            'display': 'none',
+          }
+        },
+        xAxis: {
+          categories: drupalSettings.note.lessTouchesTotalHome.categorie,
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Ballons touchés',
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: ' ballons touchés'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: drupalSettings.note.lessTouchesTotalHome.serieName,
+            data: drupalSettings.note.lessTouchesTotalHome.serie,
             color: '#D0021B'
           },
         ]
       });
-      Highcharts.chart('container-touches-away', {
+      Highcharts.chart('container-less-touches-away', {
         chart: {
           type: 'column'
         },
         title: {
-          text: 'Les joueurs ayant touché le plus de ballons',
+          text: 'Les joueurs ayant touché le moins de ballons',
           style: {
             'display': 'none',
           }
         },
         xAxis: {
-          categories: drupalSettings.note.touchesTotalAway.categorie,
+          categories: drupalSettings.note.lessTouchesTotalAway.categorie,
           title: {
             text: null
           }
@@ -186,9 +360,359 @@
         },
         series: [
           {
-            name: drupalSettings.note.touchesTotalAway.serieName,
-            data: drupalSettings.note.touchesTotalAway.serie,
+            name: drupalSettings.note.lessTouchesTotalAway.serieName,
+            data: drupalSettings.note.lessTouchesTotalAway.serie,
+            color: '#022fd0'
+          },
+        ]
+      });
+
+      // DUELS
+      Highcharts.chart('most-duels-won-all', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Les joueurs ayant gagnés le plus de duels',
+          style: {
+            'display': 'none',
+          }
+        },
+        xAxis: {
+          categories: drupalSettings.note.mostDuelsWonTotal.categorie,
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Duels gagnés',
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: ' duels gagnés'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: drupalSettings.note.mostDuelsWonTotal.serieName,
+            data: drupalSettings.note.mostDuelsWonTotal.serie,
+            color: '#000000'
+          },
+        ]
+      });
+      Highcharts.chart('most-duels-won-home', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Les joueurs ayant gagnés le plus de duels',
+          style: {
+            'display': 'none',
+          }
+        },
+        xAxis: {
+          categories: drupalSettings.note.mostDuelsWonTotalHome.categorie,
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Duels gagnés',
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: ' duels gagnés'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: drupalSettings.note.mostDuelsWonTotalHome.serieName,
+            data: drupalSettings.note.mostDuelsWonTotalHome.serie,
             color: '#D0021B'
+          },
+        ]
+      });
+      Highcharts.chart('most-duels-won-away', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Les joueurs ayant gagnés le plus de duels',
+          style: {
+            'display': 'none',
+          }
+        },
+        xAxis: {
+          categories: drupalSettings.note.mostDuelsWonTotalAway.categorie,
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Duels gagnés',
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: ' duels gagnés'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: drupalSettings.note.mostDuelsWonTotalAway.serieName,
+            data: drupalSettings.note.mostDuelsWonTotalAway.serie,
+            color: '#022fd0'
+          },
+        ]
+      });
+      Highcharts.chart('most-duels-lost-all', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Les joueurs ayant perdus le plus de duels',
+          style: {
+            'display': 'none',
+          }
+        },
+        xAxis: {
+          categories: drupalSettings.note.mostDuelsLostTotal.categorie,
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Duels perdus',
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: ' duels perdus'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: drupalSettings.note.mostDuelsLostTotal.serieName,
+            data: drupalSettings.note.mostDuelsLostTotal.serie,
+            color: '#000000'
+          },
+        ]
+      });
+      Highcharts.chart('most-duels-lost-home', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Les joueurs ayant perdus le plus de duels',
+          style: {
+            'display': 'none',
+          }
+        },
+        xAxis: {
+          categories: drupalSettings.note.mostDuelsLostTotalHome.categorie,
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Duels perdus',
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: ' duels perdus'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: drupalSettings.note.mostDuelsLostTotalHome.serieName,
+            data: drupalSettings.note.mostDuelsLostTotalHome.serie,
+            color: '#D0021B'
+          },
+        ]
+      });
+      Highcharts.chart('most-duels-lost-away', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Les joueurs ayant perdus le plus de duels',
+          style: {
+            'display': 'none',
+          }
+        },
+        xAxis: {
+          categories: drupalSettings.note.mostDuelsLostTotalAway.categorie,
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'Duels perdus',
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: ' duels perdus'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: drupalSettings.note.mostDuelsLostTotalAway.serieName,
+            data: drupalSettings.note.mostDuelsLostTotalAway.serie,
+            color: '#022fd0'
           },
         ]
       });
@@ -198,7 +722,7 @@
   Drupal.behaviors.divTerrainHeight = {
     attach: function (context, settings) {
       var remplacantsHeight = $('.remplacants').height();
-      $('.tab__content.data').css('margin-bottom', remplacantsHeight+100);
+      $('.tab__content.data > .terrain').css('margin-bottom', remplacantsHeight+100);
     }
   };
 
