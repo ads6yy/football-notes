d708e78b095ec9eb21c07aacac39362c870d69ed
Adsy <asimon@ipsosenso.com>
2025-11-01 15:40:27 +0100

    retrieve + backup sql

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -673,47 +673,6 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 # $settings['yaml_parser_class'] = NULL;
 
 /**
- * Trusted host configuration.
- *
- * Drupal core can use the Symfony trusted host mechanism to prevent HTTP Host
- * header spoofing.
- *
- * To enable the trusted host mechanism, you enable your allowable hosts
- * in $settings['trusted_host_patterns']. This should be an array of regular
- * expression patterns, without delimiters, representing the hosts you would
- * like to allow.
- *
- * For example:
- * @code
- * $settings['trusted_host_patterns'] = [
- *   '^www\.example\.com$',
- * ];
- * @endcode
- * will allow the site to only run from www.example.com.
- *
- * If you are running multisite, or if you are running your site from
- * different domain names (eg, you don't redirect http://www.example.com to
- * http://example.com), you should specify all of the host patterns that are
- * allowed by your site.
- *
- * For example:
- * @code
- * $settings['trusted_host_patterns'] = [
- *   '^example\.com$',
- *   '^.+\.example\.com$',
- *   '^example\.org$',
- *   '^.+\.example\.org$',
- * ];
- * @endcode
- * will allow the site to run off of all variants of example.com and
- * example.org, with all subdomains included.
- */
-$settings['trusted_host_patterns'] = [
-  '^local-adsyfootballnotes\.com$',
-];
-
-
-/**
  * The default list of directories that will be ignored by Drupal's file API.
  *
  * By default ignore node_modules and bower_components folders to avoid issues
@@ -761,9 +720,9 @@ $settings['entity_update_backup'] = TRUE;
 $settings['migrate_node_migrate_type_classic'] = FALSE;
 
 $databases['default']['default'] = array (
-  'database' => 'adsyfootballnotes',
-  'username' => 'adsyfootball',
-  'password' => 'adsyfootballnotespassword',
+  'database' => 'football-notes',
+  'username' => 'drupaluser',
+  'password' => 'drupaluserpassword123',
   'prefix' => '',
   'host' => 'mariadb',
   'port' => '3306',
